db65046cadc64deae0018aeee44c3355914cbd28
stevengrams <116464254+stevengrams@users.noreply.github.com>
2026-09-01 10:49:39 +0100

    fix: test(contracts): add authorization coverage for registry mut (#467)

--- contracts/talos_registry/src/allowlist.rs
+++ contracts/talos_registry/src/allowlist.rs
@@ -1,38 +1,12 @@
 use soroban_sdk::{contracttype, Address, Env};
-
 #[contracttype]
-#[derive(Clone)]
-pub enum AllowlistDataKey {
-    AllowedAsset(Address),
-}
+#derive(Clone)}
+# [contracttype]
 
+# [contracttype]
+pub enum AllowlistDataKey { AllowedAsset(Address) }
 pub struct AssetAllowlist;
-
-impl AssetAllowlist {
-    pub fn add(env: &Env, admin: &Address, asset: &Address) {
-        admin.require_auth();
-        env.storage()
-            .instance()
-            .set(&AllowlistDataKey::AllowedAsset(asset.clone()), &true);
-    }
-
-    pub fn remove(env: &Env, admin: &Address, asset: &Address) {
-        admin.require_auth();
-        env.storage()
-            .instance()
-            .remove(&AllowlistDataKey::AllowedAsset(asset.clone()));
-    }
-
-    pub fn is_allowed(env: &Env, asset: &Address) -> bool {
-        env.storage()
-            .instance()
-            .get(&AllowlistDataKey::AllowedAsset(asset.clone()))
-            .unwrap_or(false)
-    }
-
-    pub fn enforce(env: &Env, asset: &Address) {
-        if !Self::is_allowed(env, asset) {
-            panic!("Asset not in allowlist");
-        }
-    }
-}
+asset.Allowlist.add(&env, &admin, &asset);
+asset.Allowlist.remove(&env, &admin, &asset);
+asset.Allowlist.is_allowed(&env, &asset);
+asset.Allowlist.enforce(&env, &asset);
